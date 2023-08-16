1fdadb5c3382569d37f0424a44a243cb3e4814e3
Martha Nisa <nashy.nisa@gmail.com>
2023-08-16 21:20:30 +0300

    Initial Commit

---
+++ TouristNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouristNewsApp
+//
+//  Created by Martha Nashipae on 8/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
